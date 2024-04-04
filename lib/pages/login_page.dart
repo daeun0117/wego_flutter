3bf52481270f0cb399d8e0da3c01b8ddf8aeeb6c
daeun0117 <dyun60988@gmail.com>
2024-04-04 20:39:46 +0900

    second commit

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -61,32 +61,3 @@ class Login extends StatelessWidget {
     );
   }
 }
-
-/*Future<void> loginKakao() async {
-  if (await isKakaoTalkInstalled()) {
-    try {
-      await UserApi.instance.loginWithKakaoTalk();
-      print('카카오톡으로 로그인 성공');
-    } catch (error) {
-      print('카카오톡으로 로그인 실패 $error');
-
-      if (error is PlatformException && error.code == 'CANCELED') {
-        return;
-      }
-      try {
-        await UserApi.instance.loginWithKakaoAccount();
-        print('카카오계정으로 로그인 성공');
-      } catch (error) {
-        print('카카오계정으로 로그인 실패 $error');
-      }
-    }
-  } else {
-    try {
-      await UserApi.instance.loginWithKakaoAccount();
-      print('카카오계정으로 로그인 성공');
-    } catch (error) {
-      print('카카오계정으로 로그인 실패 $error');
-    }
-  }
-}
-*/
